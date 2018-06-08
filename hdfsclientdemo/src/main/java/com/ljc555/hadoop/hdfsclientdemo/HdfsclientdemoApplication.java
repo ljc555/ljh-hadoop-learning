4cd9632fe0b9457a23bbd255047a0f7bcee68a08
Li <jiangli@deloitte.com.cn>
2018-06-08 10:31:49 +0800

    add lucene module

---
+++ hdfsclientdemo/src/main/java/com/ljc555/hadoop/hdfsclientdemo/HdfsclientdemoApplication.java
@@ -0,0 +1,12 @@
+package com.ljc555.hadoop.hdfsclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfsclientdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfsclientdemoApplication.class, args);
+	}
+}
